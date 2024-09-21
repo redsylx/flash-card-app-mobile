34b0bac6ae7cfe50e79be8be7c4915e3369a918d
redsylx <fakhrialbari.red@gmail.com>
2024-09-21 22:11:30 +0700

    adjust: ui ux on history page

--- app/(tabs)/history/index.tsx
+++ app/(tabs)/history/index.tsx
@@ -4,6 +4,7 @@ import { IPaginationResult } from "@/interfaces/IPaginationResult";
 import { ITransactionActivity } from "@/interfaces/ITransactionActivity";
 import { serviceTransactionGetList } from "@/services/ServiceTransaction";
 import { useAccount, useStoreTransaction } from "@/store";
+import { FontAwesome } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { useEffect } from "react";
 import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
@@ -26,7 +27,35 @@ export default () => {
     text: {
       fontFamily: 'font-regular',
       color: theme.text,
-    }
+    },
+    time: {
+      fontFamily: 'font-regular',
+      color: theme.text,
+      fontSize: 12,
+    },
+    card: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: theme.subAlt,
+      borderRadius: 8,
+    },
+    textContainer: {
+      flex: 1,
+      marginLeft: 10,
+    },
+    title: {
+      color: theme.text,
+      fontSize: 12,
+      fontFamily: 'font-bold'
+    },
+    points: {
+      color: '#fff',
+      fontSize: 12,
+      fontFamily: 'font-regular'
+    },
+    deleteButton: {
+      paddingHorizontal: 20,
+    },
   })
 
   const getTransactions = async () => {
@@ -51,14 +80,21 @@ export default () => {
         data={transaction.items}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View>
-            <Text style={styles.text}>{item.createdTime.toString()}</Text>
-            <Text style={styles.text}>{item.id}</Text>
-            <Text style={styles.text}>Item : {item.totalItem}</Text>
-            <Text style={styles.text}>Point : {item.totalPoint}</Text>
-            <Pressable onPress={() => viewDetail(item)}>
-              <Text style={styles.text}>View Detail</Text>
-            </Pressable>
+          <View style={{ marginBottom: 20, marginHorizontal: 20 }}>
+            <View style={styles.card}>
+              <View style={styles.textContainer}>
+                <Text style={styles.title}>{item.id}</Text>
+                <Text style={styles.time}>{item.createdTime.toString()}</Text>
+                <View style={{ flex: 2, flexDirection: "row", justifyContent: 'space-between',}}>
+                <Text style={styles.text}>{item.totalItem} items</Text>
+                <Text style={styles.points}>{item.totalPoint} pts</Text>
+                </View>
+              </View>
+              <Pressable style={styles.deleteButton} onPress={() => viewDetail(item)}>
+                <FontAwesome size={28} name="eye" color={theme.main} />
+              </Pressable>
+            </View>
+
           </View>
         )}
       />
